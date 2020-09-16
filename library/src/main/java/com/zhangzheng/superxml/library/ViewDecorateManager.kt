65c7ec449a5e189d0d5cf772f617082be1679bc2
zhangzheng <zheng.zhang5>
2020-09-16 17:39:33 +0800

    圆角添加成功

--- library/src/main/java/com/zhangzheng/superxml/library/ViewDecorateManager.kt
+++ library/src/main/java/com/zhangzheng/superxml/library/ViewDecorateManager.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.View
-import com.zhangzheng.superxml.library.decorate.IDecorateView
 import com.zhangzheng.superxml.library.decorate.RadiusDecorate
+import com.zhangzheng.superxml.library.decorate.SrcRadiusDecorate
 
 internal object ViewDecorateManager {
 
-    private val decorateList = arrayListOf<IDecorateView>(
-        RadiusDecorate()
+    private val decorateList = arrayListOf(
+        RadiusDecorate(),
+        SrcRadiusDecorate()
     )
 
     fun hasDecorate(context: Context, attributeSet: AttributeSet): Boolean {
---
+++ library/src/main/java/com/zhangzheng/superxml/library/ext/ViewRadiusExt.kt
@@ -0,0 +1,72 @@
+package com.zhangzheng.superxml.library.ext
+
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.view.View
+import android.widget.ImageView
+
+
+fun View.setRadius(radius: Float) {
+    val backgroundColor = getBackgroundColor(this)
+    val background = getBackground(this)
+    if (backgroundColor != null) {
+        this.background = setRoundRectBgByColorValue(this, backgroundColor, radius)
+    }else if(background!=null){
+        this.background = BitmapDrawable(toRoundCorner(background,radius))
+    }
+}
+
+fun View.setSrcRadius(radius: Float){
+
+    if(this is ImageView){
+        val src = drawable
+        if(src is BitmapDrawable){
+            setImageBitmap(toRoundCorner(src.bitmap,radius))
+        }
+    }
+}
+
+private fun setRoundRectBgByColorValue(view: View, color: Int, radius: Float) :Drawable{
+    return GradientDrawable().also {
+        it.shape = GradientDrawable.RECTANGLE
+        it.cornerRadius = radius
+        it.setColor(color)
+    }
+}
+
+
+private fun getBackgroundColor(view: View): Int? {
+    var bgColor: Int? = null
+    if (view.background is ColorDrawable) {
+        val colorDrawable = view.background as ColorDrawable
+        bgColor = colorDrawable.color
+    }
+    return bgColor
+}
+
+private fun getBackground(view: View): Bitmap? {
+    val background = view.background
+    return if (background is BitmapDrawable) {
+        background.bitmap
+    } else {
+        null
+    }
+}
+
+private fun toRoundCorner(bitmap: Bitmap, pixels: Float): Bitmap? {
+    val output =
+        Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(output)
+    val paint = Paint()
+    val rect = Rect(0, 0, bitmap.width, bitmap.height)
+    val rectF = RectF(rect)
+    paint.isAntiAlias = true
+    canvas.drawARGB(0, 0, 0, 0)
+    canvas.drawRoundRect(rectF, pixels/2, pixels/2, paint)
+    paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+    canvas.drawBitmap(bitmap, rect, rect, paint)
+    return output
+}
